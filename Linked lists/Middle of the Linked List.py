7e1cd4b3dfb7674903015215269b94e3090089ca
sreeyachandra <125431397+sreeyachandra@users.noreply.github.com>
2024-03-11 19:56:53 +0530

    Create Middle of the Linked List.py

---
+++ Linked lists/Middle of the Linked List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        fast=slow=head
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
